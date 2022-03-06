7d5da14cb13184a29b63d9974cd29278c07719d0
Scott Fergsuon <scottf174@gmail.com>
2022-03-06 18:11:31 +1100

    create diploid fasta by cat'ing hap1 & hap2 together

--- hifiasm.sh
+++ hifiasm.sh
@@ -7,3 +7,5 @@ ${hifiasmDir}/hifiasm -t threads -o $(basename $fastq .fastq.gz) $fastq
 awk '/^S/{print ">"$2;print $3}' $(basename $fastq .fastq.gz).bp.hap1.p_ctg.gfa > hap1.fasta
 awk '/^S/{print ">"$2;print $3}' $(basename $fastq .fastq.gz).bp.hap2.p_ctg.gfa > hap2.fasta
 awk '/^S/{print ">"$2;print $3}' $(basename $fastq .fastq.gz).bp.p_ctg.gfa      > unphased.fasta
+
+cat hap1.fasta hap2.fasta > diploid.fasta
