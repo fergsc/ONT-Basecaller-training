0d2d9aee270e36da96d2534449c3b31d9ccd2363
Scott Fergsuon <scottf174@gmail.com>
2022-03-06 19:07:00 +1100

    modified train.sh so that it will iterate over a list of training sets

--- train.sh
+++ train.sh
@@ -1,25 +1,45 @@
-basecallingOutput="/path/to/bonito-basecalling"
+basecallingOutput="/path/to/bonito-basecalling/1 /path/to/bonito-basecalling/2" # eg. for multiple sub-devided training sets
+basecallingOutput="/path/to/bonito-basecalling" # eg. for single training set
+
 bonitoDir="/path/to/bonito"
 
 cudaDevice="cuda:0"
-epochs=15      # number of training cycles
+epochs=12      # number of training cycles
 batch_size=100 # number of training reads utilized in one iteration.
 num_chunks=0   # Splits the reads into lengths of size X. 0 = don't split
-learning_rate=4e-4 # tuning parameter for optimization of learning algorithm.
-                   # Step size at each epoch
+learning_rate=4e-4 # Step size at each epoch
                    # to big = overshoot convergent point
                    # to small = slow learn.
 
 # batch and chunk = resource optimisation.
 
+first=1
+for run in $basecallingOutput
+do
+    if [ $first == 1 ]
+    then
+        ${bonitoDir}/bonito train \
+            --device $cudaDevice           \
+            --epochs $epochs               \
+            --lr $learning_rate            \
+            --batch $batch_size            \
+            --directory $run \
+            $(basename $run)~trained
+            #--chunks $num_chunks # using 0 -> dont need
 
-${bonitoDir}/bonito train \
-    --device $cudaDevice           \
-    --epochs $epochs               \
-    --lr $learning_rate            \
-    --batch $batch_size            \
-    --directory $basecallingOutput \
-    $(basename $basecallingOutput)~trained
+        preRun=$(basename $run)~trained
+        first=0
+    else
+        ${bonitoDir}/bonito train \
+            --pretrained $preRun           \
+            --device $cudaDevice           \
+            --epochs $epochs               \
+            --lr $learning_rate            \
+            --batch $batch_size            \
+            --directory $run \
+            $(basename $run)~trained
+            #--chunks $num_chunks # using 0 -> dont need
 
-#--chunks $num_chunks # using 0 -> dont need
-
+        preRun=$(basename $run)~trained
+    fi
+done
