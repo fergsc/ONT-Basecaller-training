6f58a71e06912515a2a94209a72a36b0506112b5
Scott Ferguson <33704509+fergsc@users.noreply.github.com>
2022-05-24 15:49:27 +1000

    Update and rename trainingStats to readIdentity.sh

--- readIdentity.sh
+++ readIdentity.sh
@@ -10,3 +10,7 @@ ${minimap2Dir}/minimap2 -t $threads -ax map-ont $reference $reads \
     | ${samtoolsDir}/samtools view -b > mapped_reads.bam
 
 ${promixio}/stats_from_bam -t $threads -o $(basename $reference)~$(basename $reads).csv mapped_reads.bam
+
+echo "basecaller,length,identity" > $(basename $reference)~$(basename $reads).csv
+awk -v OFS="," -v R=$(basename $reference)~$(basename $reads) 'NR > 1 {print R,$12,$17}' $(basename $reference)~$(basename $reads).csv \
+    >> $(basename $reference)~$(basename $reads).csv
